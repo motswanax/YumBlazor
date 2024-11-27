6fa85dcfcb70ce41807aef98673de9ee9e229d96
Baiketli Dijo <baiketlid@gmail.com>
2024-11-27 07:35:35 +0200

    Implemented shopping cart repository.

---
+++ YumBlazor.Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,10 @@
+﻿using YumBlazor.Entities;
+
+namespace YumBlazor.Contracts;
+public interface IShoppingCartRepository
+{
+    Task<bool> ClearCartAsync(string? userId);
+    Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
+    Task<int> GetTotalCartCartCountAsync(string? userId);
+    Task<bool> UpdateCartAsync(string userId, int productId, int updateBy);
+}
