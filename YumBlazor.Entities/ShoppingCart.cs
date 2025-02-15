a6eff35fd0efd785a59220dea65e1fe6cc9dac1a
Baiketli Dijo <baiketlid@gmail.com>
2025-02-15 08:48:44 +0200

    Added social login packages.

--- YumBlazor.Entities/ShoppingCart.cs
+++ YumBlazor.Entities/ShoppingCart.cs
@@ -4,15 +4,15 @@ namespace YumBlazor.Entities;
 public class ShoppingCart
 {
     public int Id { get; set; }
-    public string UserId { get; set; }
+    public required string UserId { get; set; }
 
     [ForeignKey("UserId")]
-    public ApplicationUser User { get; set; }
+    public ApplicationUser? User { get; set; }
 
     public int ProductId { get; set; }
 
     [ForeignKey("ProductId")]
-    public Product Product { get; set; }
+    public Product? Product { get; set; }
 
     public int Count { get; set; }
 }
